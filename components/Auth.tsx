9beb3f292b2bd51d860aadf3a7b7a470e63a8f69
ZivCarmi <carmi.ziv@gmail.com>
2025-06-16 21:12:44 +0300

    changed responseType for auth

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -8,127 +8,101 @@ import { Text } from "~/components/ui/text";
 import { useAuth } from "~/context/auth-context";
 import { supabase } from "~/lib/supabase";
 
-const CLIENT_ID =
-  Platform.OS === "ios"
-    ? process.env.EXPO_PUBLIC_IOS_CLIENT_ID
-    : process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID;
-const SCOPES = ["openid", "https://www.googleapis.com/auth/userinfo.email"];
-const REDIRECT_URI = makeRedirectUri({
+WebBrowser.maybeCompleteAuthSession();
+
+const config = {
+  androidClientId: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID,
+  iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID,
+  webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
+};
+
+const redirectUri = makeRedirectUri({
   scheme: "com.zivcarmi.pokerapp",
 });
 
+const createSessionFromUrl = async (url: string) => {
+  const { params, errorCode } = QueryParams.getQueryParams(url);
+
+  if (errorCode) throw new Error(errorCode);
+  const { access_token, refresh_token } = params;
+
+  if (!access_token) return;
+
+  const { data, error } = await supabase.auth.setSession({
+    access_token,
+    refresh_token,
+  });
+  console.log("Session data:", data.session);
+
+  if (error) throw error;
+  return data.session;
+};
+
 export function performGoogleLogin() {
   const [request, response, promptAsync] = useAuthRequest({
-    clientId: CLIENT_ID,
-    scopes: SCOPES,
-    redirectUri: REDIRECT_URI,
+    responseType: "id_token", // without it - auth works but im not getting anything from result with promptAsync()... need to figure out
+    clientId:
+      Platform.OS === "android"
+        ? config.androidClientId
+        : Platform.OS === "ios"
+        ? config.iosClientId
+        : config.webClientId,
+    redirectUri,
+    scopes: ["openid", "profile", "email"],
+    extraParams: {
+      nonce: "random-nonce", // חובה כשמשתמשים ב-id_token
+    },
   });
 
   const signIn = async () => {
-    const result = await promptAsync();
+    if (Platform.OS === "web") {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: "google",
+        options: {
+          redirectTo: redirectUri,
+          skipBrowserRedirect: true,
+        },
+      });
 
-    console.log(result);
+      console.log(error, data);
+
+      if (error) throw error;
+
+      const res = await WebBrowser.openAuthSessionAsync(
+        data?.url ?? "",
+        redirectUri
+      );
+
+      if (res.type === "success") {
+        const { url } = res;
+        await createSessionFromUrl(url);
+      }
+    } else {
+      const result = await promptAsync();
+
+      console.log("result.type: ", result.type);
+
+      if (result.type === "success") {
+        console.log(result.authentication);
+
+        const idToken = result.authentication?.idToken;
+
+        if (idToken) {
+          const { error } = await supabase.auth.signInWithIdToken({
+            provider: "google",
+            token: idToken,
+          });
+
+          if (error) {
+            console.error("Supabase login error:", error.message);
+          }
+        }
+      }
+    }
   };
 
   return { request, response, signIn };
 }
-// WebBrowser.maybeCompleteAuthSession();
-
-// const discovery = {
-//   authorizationEndpoint: "https://accounts.google.com/o/oauth2/v2/auth",
-//   tokenEndpoint: "https://oauth2.googleapis.com/token",
-//   revocationEndpoint: "https://accounts.google.com/o/oauth2/revoke",
-// };
-
-// const config = {
-//   androidClientId: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID,
-//   iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID,
-//   webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
-// };
-
-// const redirectUri = makeRedirectUri({
-//   scheme: "com.zivcarmi.pokerapp",
-// });
-
-// // console.log(makeRedirectUri({ scheme: "com.zivcarmi.pokerapp" }));
-
-// const createSessionFromUrl = async (url: string) => {
-//   const { params, errorCode } = QueryParams.getQueryParams(url);
-
-//   if (errorCode) throw new Error(errorCode);
-//   const { access_token, refresh_token } = params;
-
-//   if (!access_token) return;
-
-//   const { data, error } = await supabase.auth.setSession({
-//     access_token,
-//     refresh_token,
-//   });
-//   console.log("Session data:", data.session);
-
-//   if (error) throw error;
-//   return data.session;
-// };
-
-// export function performGoogleLogin() {
-//   const [request, response, promptAsync] = useAuthRequest(
-//     {
-//       responseType: ResponseType.IdToken,
-//       clientId:
-//         Platform.OS === "android"
-//           ? config.androidClientId
-//           : Platform.OS === "ios"
-//           ? config.iosClientId
-//           : config.webClientId,
-//       redirectUri,
-//       scopes: ["profile", "email"],
-//     }
-//   );
-
-//   const signIn = async () => {
-//     if (Platform.OS === "web") {
-//       const { data, error } = await supabase.auth.signInWithOAuth({
-//         provider: "google",
-//         options: {
-//           redirectTo: redirectUri,
-//           skipBrowserRedirect: true,
-//         },
-//       });
-
-//       console.log(error, data);
-
-//       if (error) throw error;
-
-//       const res = await WebBrowser.openAuthSessionAsync(
-//         data?.url ?? "",
-//         redirectUri
-//       );
-
-//       if (res.type === "success") {
-//         const { url } = res;
-//         await createSessionFromUrl(url);
-//       }
-//     } else {
-//       const result = await promptAsync();
-
-//       if (result?.type === "success") {
-//         const idToken = result.authentication?.idToken;
-//         if (idToken) {
-//           const { error } = await supabase.auth.signInWithIdToken({
-//             provider: "google",
-//             token: idToken,
-//           });
-
-//           if (error) {
-//             console.error("Supabase login error:", error.message);
-//           }
-//         }
-//       }
-//     }
-//   };
-
-//   return { request, response, signIn };
-// }
 
 export default function Auth() {
   const { signIn } = performGoogleLogin();
